e31d86ad176ffac721adaa3266e6c8dadf263fa0
valmaiimtiyaz <valmaityz30@gmail.com>
2025-11-27 17:40:29 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-const pool = new Pool ({
-  user : process.env.DB_USER,
-  host : process.env.DB_HOST,
-  database : process.env.DB_NAME,
-  password : process.env.DB_PASSWORD,
-  port : process.env.DB_PORT
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL || process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false 
+  }
 });
 
 module.exports = pool;
