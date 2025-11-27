699399d85bddf89aad92f27bdcc2c6fdcf11850d
valmaiimtiyaz <valmaityz30@gmail.com>
2025-11-27 17:51:32 +0700

    Move addUser route to /register endpoint

--- routes/snapRoutes.js
+++ routes/snapRoutes.js
@@ -2,17 +2,16 @@ const express = require("express");
 const {
   getAllUsers,
   getUserById,
-  addUser,
+  addUser, 
   updateUser,
   deleteUser,
 } = require("../controllers/snapControllers");
 
 const router = express.Router();
 
-// Users routes
+router.post("/register", addUser);
 router.get("/users", getAllUsers);
 router.get("/users/:id", getUserById);
-router.post("/users", addUser);
 router.put("/users/:id", updateUser);
 router.delete("/users/:id", deleteUser);
 
