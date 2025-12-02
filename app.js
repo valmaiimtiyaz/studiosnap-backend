20764acc90d222e4b08750e7061a2f2b34be5b92
valmaiimtiyaz <valmaityz30@gmail.com>
2025-12-03 00:43:08 +0700

    update

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use("/api", snapRoutes);
 
--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const app = require("./app"); 
+const app = require("./app");
 
 app.use((err, req, res, next) => {
-  console.error(err.stack); 
+  console.error(err.stack);
   if (res.headersSent) {
     return next(err);
-  } 
+  }
   res.status(500).json({
     status: "error",
     code: 500,
-    message: "Internal Server Error. Check server logs.",
+    message: "Internal Server Error: " + err.message,
   });
 });
+
+if (require.main === module) {
+  const PORT = process.env.PORT || 3030;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
 
 module.exports = app;
