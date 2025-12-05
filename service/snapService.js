4872c242acaa1ac45a945584affa7b1af2334d03
valmaiimtiyaz <valmaityz30@gmail.com>
2025-12-06 00:52:52 +0700

    update

--- service/snapService.js
+++ service/snapService.js
@@ -28,20 +28,19 @@ async function addUser(data) {
 
 async function updateUser(id, data) {
   const { username, email, password } = data;
-
-  if (password) {
-    const result = await pool.query(
-      `UPDATE users SET username = $1, email = $2, password = $3 WHERE id = $4 RETURNING *`,
-      [username, email, password, id]
-    );
-    return result.rows[0];
-  } else {
+  if (!password) {
     const result = await pool.query(
       `UPDATE users SET username = $1, email = $2 WHERE id = $3 RETURNING *`,
       [username, email, id]
     );
     return result.rows[0];
   }
+  
+  const result = await pool.query(
+    `UPDATE users SET username = $1, email = $2, password = $3 WHERE id = $4 RETURNING *`,
+    [username, email, password, id]
+  );
+  return result.rows[0];
 }
 
 async function deleteUser(id) {
